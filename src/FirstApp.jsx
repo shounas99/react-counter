388836a8118ab46c48c109b3143a0f30a5f0c882
Cris <to.aguilar.rodriguez.cristina@gmail.com>
2025-01-30 14:27:14 -0600

    feat(props): add props

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,6 +1,28 @@
 //shortcut - rafc 
-export const FirstApp = () => {
+const newMessage = {
+    title: 'Hi',
+    message: 'Hola mundo',
+}
+const getResult = () => {
+    return 1 + 1
+}
+
+
+export const FirstApp = ({ title = 'Title', subtitle2 = "second" }) => { /*can I either require a prop to be passed or provide a default value*/
+    // we could add constants, variables, or functions within functional components. However, when 
+    // a component re-renders, its memory space is re-initialized. Therefore, we need to avoid 
+    // writing them directly inside the component's body, even if we need to handle button clicks,
+    // execute functions, or manage state changes, all of which may require those constants, variables, or functions.
+
+
+
   return (
-    <h1>Cris</h1>
+    <>
+    <h1>{ title }</h1>
+    <h1>{ subtitle2 }</h1>
+    <h1> { getResult() }</h1>
+        <code>{ JSON.stringify(newMessage)  }</code>
+        <p>Im subtitle</p>
+    </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-// import { HelloWorldApp } from './HelloWorldApp'
 import { FirstApp } from './FirstApp'
-
+import './styles.css';
 
 ReactDOM.createRoot( document.getElementById('root')).render(
     <StrictMode>
-        <FirstApp />
+        <FirstApp title="Hi!" subtitle2="Subtitle 2" />
     </StrictMode>
 )
