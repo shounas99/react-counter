ff2af2ada197cb399d443e4f9e6b741f63636cdd
Cris <to.aguilar.rodriguez.cristina@gmail.com>
2025-01-30 15:06:49 -0600

    feat(props): add prop-types
    - add required or not propTypes

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,4 +1,7 @@
-//shortcut - rafc 
+//shortcut - rafc
+
+import PropTypes from 'prop-types'
+
 const newMessage = {
     title: 'Hi',
     message: 'Hola mundo',
@@ -8,21 +11,25 @@ const getResult = () => {
 }
 
 
-export const FirstApp = ({ title = 'Title', subtitle2 = "second" }) => { /*can I either require a prop to be passed or provide a default value*/
+export const FirstApp = ({ title, subtitle }) => { /*can I either require a prop to be passed or provide a default value*/
     // we could add constants, variables, or functions within functional components. However, when 
     // a component re-renders, its memory space is re-initialized. Therefore, we need to avoid 
     // writing them directly inside the component's body, even if we need to handle button clicks,
     // execute functions, or manage state changes, all of which may require those constants, variables, or functions.
-
-
+  
 
   return (
     <>
     <h1>{ title }</h1>
-    <h1>{ subtitle2 }</h1>
+    <h1>{ subtitle }</h1>
     <h1> { getResult() }</h1>
         <code>{ JSON.stringify(newMessage)  }</code>
         <p>Im subtitle</p>
     </>
   )
+}
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.number
 }
