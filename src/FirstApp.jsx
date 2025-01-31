39e2cad95cec386047a26ee19d58b125e1df8caf
Cris <to.aguilar.rodriguez.cristina@gmail.com>
2025-01-31 15:19:28 -0600

    fix(indent): update indent in first app file

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -12,17 +12,17 @@ const getResult = () => {
 
 
 export const FirstApp = ({ title = 'without title', subtitle = 'without subtitle'}) => { /*can I either require a prop to be passed or provide a default value*/
-    // we could add constants, variables, or functions within functional components. However, when 
-    // a component re-renders, its memory space is re-initialized. Therefore, we need to avoid 
-    // writing them directly inside the component's body, even if we need to handle button clicks,
-    // execute functions, or manage state changes, all of which may require those constants, variables, or functions.
+  // we could add constants, variables, or functions within functional components. However, when 
+  // a component re-renders, its memory space is re-initialized. Therefore, we need to avoid 
+  // writing them directly inside the component's body, even if we need to handle button clicks,
+  // execute functions, or manage state changes, all of which may require those constants, variables, or functions.
   
 
   return (
     <>
-    <h1>{ title }</h1>
-    <p>{ subtitle }</p>
-    <h1> { getResult() }</h1>
+      <h1>{ title }</h1>
+      <p>{ subtitle }</p>
+      <h1> { getResult() }</h1>
       <code>{ JSON.stringify(newMessage)  }</code>
       <p>Im subtitle</p>
     </>
