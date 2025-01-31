31e8886d94f4e925bdffc2ae8df10a89bec58189
Cris <to.aguilar.rodriguez.cristina@gmail.com>
2025-01-31 10:50:23 -0600

    feat(hooks): add hooks for functionality to change state

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import { FirstApp } from './FirstApp'
+import { CounterApp } from './CounterApp';
 import './styles.css';
 
 ReactDOM.createRoot( document.getElementById('root')).render(
     <StrictMode>
-        <FirstApp />
+        <CounterApp value={ 0 } />
     </StrictMode>
 )
