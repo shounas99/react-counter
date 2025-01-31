87c75d494d86d4f29a795064201c721fd2f43c8c
Cris <to.aguilar.rodriguez.cristina@gmail.com>
2025-01-31 13:00:39 -0600

    feat(counter): add subtract and reset functionality to the counter

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,20 +1,36 @@
 import { useState } from 'react'
 import PropTypes from "prop-types"
+import confetti from 'https://cdn.skypack.dev/canvas-confetti';
 
 export const CounterApp = ({ value }) => {
+
+
   const [ counter , setCounter ] = useState( value );
   const handleAdd = () => { 
-    setCounter( value + 1 )
+    setCounter( counter + 1 )
+    if ((counter + 1)% 10 === 0) {
+      confetti();
+    }
   }
+  const handleSubtract = () => {
+    setCounter( counter - 1)
+    if ((counter - 1) % 10 === 0) {
+      confetti();
+    }
+    if(counter - 1 === 0){
+      handleReset()
+    }
+  }
+  const handleReset = () => setCounter(value)
 
   return (
     <>
-      <h1>CounterApp</h1>
+      <h1>Counter App</h1>
       <h2>{ counter }</h2>
 
-      <button onClick={ handleAdd }>
-        +1 
-      </button>
+      <button onClick={ handleAdd }> +1 </button>
+      <button onClick={ handleSubtract }> -1 </button>
+      <button onClick={ handleReset }> Reset </button>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import { CounterApp } from './CounterApp';
-import './styles.css';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { CounterApp } from "./CounterApp";
+import "./styles.css";
 
-ReactDOM.createRoot( document.getElementById('root')).render(
-    <StrictMode>
-        <CounterApp value={ 0 } />
-    </StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <CounterApp value={20} />
+  </StrictMode>
+);
