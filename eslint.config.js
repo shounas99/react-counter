eff5b20448d901aae0806682a9dd572acd5c55de
Cris <to.aguilar.rodriguez.cristina@gmail.com>
2025-02-04 12:45:33 -0600

    chore(babel): add jest and babel configurations

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,9 @@ export default [
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    env: {
+      "jest": true
+    },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
